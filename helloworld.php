7c5ce33ee6a90a8b2ec3265b77c51f9fb4fbcd95
elispihl <elispihl@greeny.cs.tlu.ee>
2016-02-11 13:28:40 +0200

    added file

---
+++ helloworld.php
@@ -0,0 +1,57 @@
+ <?php
+ 
+	//Variable starts with $
+	//string has to use quotation marks
+	// line ends with semicolon
+	$first_name = "Elis";
+	$last_name = "Pihl";
+	
+	//show code to browser
+	//for adding up numbers	+ sign
+	//for adding up strings .
+	echo $first_name." ".$last_name;
+	
+	//>greater than
+	//< smaller than
+	//>= greater than or equal
+	//<= smaller than or equal
+	//== equal
+	//!= not equal
+	
+	$age = 4;
+	
+	//if age is smaller than or equal to 6, then
+	
+	if($age <=6) {
+		//it is true
+			echo "yes you can sleep at lunchtime";
+		}else{ 
+			//it is false
+			echo "no, you are at school";
+			
+			}
+ 
+ ?>
+ 
+ <br>
+ 
+  <?php
+  
+	//loop
+	
+	for ($i=1; $i <= $age; $i=$i+1){
+		//if condition is true 
+		echo $i;
+	}
+	
+	?>
+	
+	 <br>
+	 
+	  <?php
+	   echo "Today is " .date("d.m.Y H:i");
+	 ?> 
+	  
+	
+ 
+ 
